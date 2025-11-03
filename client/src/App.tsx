49f651acdd4a419e9175fc1c75a8de97af1132fc
1hk22ai038 <49334970-1hk22ai038@users.noreply.replit.com>
2025-11-03 18:39:56 +0000

    Restored to 'a8c04958c3d8c791a7c4f7bf058b8eb1c08b783c'
    Replit-Restored-To: a8c04958c3d8c791a7c4f7bf058b8eb1c08b783c

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react";
 import NotFound from "@/pages/not-found";
 import LoginPage from "@/pages/LoginPage";
 import HomePage from "@/pages/HomePage";
-import ResultsPage from "@/pages/ResultsPage";
 import HistoryPage from "@/pages/HistoryPage";
 import FraudReportPage from "@/pages/FraudReportPage";
 import AboutPage from "@/pages/AboutPage";
@@ -47,9 +46,6 @@ function Router() {
         <Route path="/login" component={LoginPage} />
         <Route path="/home">
           {() => <ProtectedRoute component={HomePage} />}
-        </Route>
-        <Route path="/results">
-          {() => <ProtectedRoute component={ResultsPage} />}
         </Route>
         <Route path="/history">
           {() => <ProtectedRoute component={HistoryPage} />}
